c6a6c17309c0108526cc5cb25a325ade527b3fe3
孙可旺 <ribiki-master@hiroshima-u.ac.jp>
2025-04-27 22:23:20 +0900

    droupout层添加

--- app/models/model.py
+++ app/models/model.py
@@ -13,6 +13,7 @@ class SimpleCNN(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            nn.Dropout(0.25),
 
             # Block 2
             nn.Conv2d(64, 128, kernel_size=3, padding=1, bias=False),
@@ -22,6 +23,7 @@ class SimpleCNN(nn.Module):
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            nn.Dropout(0.25),
 
             # Block 3
             nn.Conv2d(128, 256, kernel_size=3, padding=1, bias=False),
@@ -31,6 +33,7 @@ class SimpleCNN(nn.Module):
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            nn.Dropout(0.25),
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
