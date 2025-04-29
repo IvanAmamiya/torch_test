1de94fcab1a8b6f8aa091955a49cce015446e248
孙可旺 <ribiki-master@hiroshima-u.ac.jp>
2025-04-30 00:12:38 +0900

    试错

--- app/models/model.py
+++ app/models/model.py
@@ -6,28 +6,28 @@ class SimpleCNN(nn.Module):
         super(SimpleCNN, self).__init__()
         self.features = nn.Sequential(
             # Block 1
-            nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(3, 96, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(96),
             nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(96, 96, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(96),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             nn.Dropout(0.25),
             nn.LocalResponseNorm(5),  # LRN 层，提升泛化能力
 
             # Block 2
-            nn.Conv2d(64, 128, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(96, 96, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(96),
             nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(96, 96, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(96),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
             nn.Dropout(0.25),
 
             # Block 3
-            nn.Conv2d(128, 256, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(96, 256, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False),
@@ -40,7 +40,7 @@ class SimpleCNN(nn.Module):
             nn.Flatten(),
             nn.Linear(256 * 4 * 4, 512),
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Dropout(0.4),
             nn.Linear(512, num_classes)
         )
 
